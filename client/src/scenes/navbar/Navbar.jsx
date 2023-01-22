afd41747ea7429e44165be3a5b3d49a5b4960e38
kuldeepSingh <singhkuldeep7850@gmail.com>
2023-01-23 01:31:59 +0530

    project done

--- client/src/scenes/navbar/Navbar.jsx
+++ client/src/scenes/navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt; 
 
-  const fullName = `${user?.firstName | "Kuldeep"} ${user?.lastName | "Singh"}`
+  const fullName = `${user?.firstName} ${user?.lastName}`
 
   console.log(isNonMobileScreens)
 
